23aaadee760ae0ed72b3f44ad1fc6826905bf66b
nguyen van bac <nguyenbac29305@gmail.com>
2026-04-08 07:27:59 +0700

    cors

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const app = express();
 
 const port = process.env.PORT||3000;
 
+const cors = require("cors");
+app.use(cors());
+
 app.use(express.json());
 
 const routeClient = require("./routes/client/index.route");
